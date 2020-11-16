eb5be3e90cc364673f3cb8e3ea1fae5312aac986
Danny Kha <danny.n.kha@gmail.com>
2020-11-15 21:35:30 -0800

    almost done

--- src/main/java/Problem3/InsertInBST.java
+++ src/main/java/Problem3/InsertInBST.java
@@ -4,21 +4,21 @@ import Problem1.TreeNode;
 
 public class InsertInBST {
     public static void insert(TreeNode<Integer> root, int valToInsert) {
-       if (valToInsert < root.val) {
-           if (root.left != null) {
-               insert(root.left, valToInsert);
-           }
-           else {
-               root.right = new TreeNode(valToInsert);
-           }
-       }
-       else if (valToInsert > root.val) {
-           if (root.right != null) {
-               insert(root.right, valToInsert);
-           }
-           else {
-               root.right = new TreeNode(valToInsert);
-           }
-       }
+        if (valToInsert < root.val) {
+            if (root.left == null) {
+                root.left = new TreeNode<>(valToInsert);
+            }
+            else {
+                insert(root.left, valToInsert);
+            }
+        }
+        else {
+            if (root.right == null) {
+                root.right = new TreeNode<>(valToInsert);
+            }
+            else {
+                insert(root.right, valToInsert);
+            }
+        }
     }
 }
--- src/test/java/Problem3Test.java
+++ src/test/java/Problem3Test.java
@@ -34,10 +34,35 @@ public class Problem3Test {
 
     @Test
     public void testInOrderTraverse() {
-        List<Integer> result = new ArrayList<>();
+        List<BSTTestCase<Integer>> testCases = getBSTTestCases();
 
-
+        int[][] expected = {
+                {1},
+                {1},
+                {1},
+                {1, 2},
+                {1, 2},
+                {1, 2},
+                {2, 4, 6},
+                {2, 4, 6},
+                {2, 4, 6},
+                {2, 4, 6},
+                {3, 5, 6, 9, 10, 13},
+                {3, 5, 6, 9, 10, 13},
+                {3, 5, 6, 9, 10, 13},
+                {3, 5, 6, 9, 10, 13},
+                {1},
+                {1},
+                {1},
+                {1}
+        };
+        for (int i = 0; i < testCases.size(); i++) {
+            BSTTestCase<Integer> testCase = testCases.get(i);
+            List<Integer> actual = inOrderTraverse(testCase.tree);
+            assertEquals(Arrays.asList(expected[i]), actual);
+        }
     }
+
 
     private static List<Integer> inOrderTraverse(TreeNode<Integer> node) {
         List<Integer> result = new ArrayList<>();
@@ -147,9 +172,10 @@ public class Problem3Test {
         //    N   N
         // homework
         // what problem can you see for insertInBst from this test case?
-        // answer:
+        // answer: all of the inserted values are greater then 1, so then all the insert values will be on the right side and the left
+        // side will not have anything.
         // discuss how you would solve it in a comment below
-        // answer:
+        // answer: A solution to have numbers on the left hand side would to insert values that are less then 1, like 0.
         root = new TreeNode<>(1);
         testCases.add(new BSTTestCase<>(root, 2, Arrays.asList(1, 2)));
         testCases.add(new BSTTestCase<>(root, 3, Arrays.asList(1, 2, 3)));
